5d19eec1ad3b861ba956635a30d04636057b7532
cho <some_days_@naver.com>
2017-06-28 16:40:51 +0900

    Init Calendar Project

---
+++ src/codesquad/calendar/Calendar.java
@@ -0,0 +1,10 @@
+package codesquad.calendar;
+
+public class Calendar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello 카렌다");
+	}
+
+}
